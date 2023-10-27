a2dc7908e0e3a8195fd207536240e97e3f28828e
BuiVanTienB2014620 <vinhb2014633@student.ctu.edu.vn>
2023-10-27 21:11:58 +0700

    update 10/27/2023

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -33,14 +33,18 @@ class ProductGridTile extends StatelessWidget {
   Widget buildGridFooterBar(BuildContext context) {
     return GridTileBar(
       backgroundColor: Colors.black87,
-      leading: IconButton(
-        icon: Icon(
-          product.isFavorite ? Icons.favorite : Icons.favorite_border,
-        ),
-        color: Theme.of(context).colorScheme.secondary,
-        onPressed: () {
-          print('Thay đổi sản phẩm yêu thích');
-          // Add your favorite toggle logic here
+      leading: ValueListenableBuilder<bool>(
+        valueListenable: product.isFavoriteListenable(),
+        builder: (ctx, isFavorite, child) {
+          return IconButton(
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              product.isFavorite = !isFavorite;
+            },
+          );
         },
       ),
       title: Text(
